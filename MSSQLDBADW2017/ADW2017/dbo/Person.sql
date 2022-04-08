27204445e102275d5718ab3d9da9a17f2b5e3a79
barisosoram <bsora1@live.maryville.edu>
2022-04-07 23:23:48 -0700

    Solution,project and schema object created and added to project

---
+++ MSSQLDBADW2017/ADW2017/dbo/Person.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE dbo.Person(
+	BusinessEntityID int NOT NULL,
+	PersonType nchar(2) NOT NULL,
+	Title nvarchar(8) NULL,
+	FirstName varchar(50) NOT NULL,
+	MiddleName varchar(50) NULL,
+	LastName varchar(50) NOT NULL,
+	Suffix nvarchar(10) NULL,
+	EmailPromotion int NOT NULL,
+	rowguid uniqueidentifier NOT NULL,
+	ModifiedDate datetime NOT NULL,
+ CONSTRAINT PK_Person PRIMARY KEY CLUSTERED 
+(
+	BusinessEntityID ASC
+))
